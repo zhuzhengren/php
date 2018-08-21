3ada151b45543cdaf2bd68c2c39c9c65abf93b30
zhuzhengren <zhuzr@yilan.tv>
2018-08-21 23:21:54 +0800

    cookie3

--- cookie.php
+++ cookie.php
@@ -1,3 +1,11 @@
 <?php 
 echo "Hello World!"; 
+setcookie("name","zhu",time()+300);
+echo $_COOKIE["name"];
+if (isset($_COOKIE["name"])){
+    echo "cookie is".$_COOKIE["name"];
+}else {
+    echo "no cookie";
+}
+echo "end";
 ?> 
