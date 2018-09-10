9c3c5227bc23250c852a504923e6cb58fced1e74
zhuzhengren <zhuzr@yilan.tv>
2018-09-10 13:12:53 +0800

    location

--- cookie.php
+++ cookie.php
@@ -1,6 +1,6 @@
 <?php 
 echo "Hello World!"; 
-setcookie("name",time(),time()+300);
+
 print_r($_COOKIE);
 if (isset($_COOKIE["name"])){
     echo "cookie is".$_COOKIE["name"];
@@ -8,7 +8,8 @@ if (isset($_COOKIE["name"])){
     echo "no cookie";
 }
 
-setcookie("name","",time()-300);
+setcookie("name",time(),time()+300);
+
 print_r($_COOKIE);
 echo "end";
 ?> 
---
+++ location.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="zh-cn">
+<head>
+    <meta charset="utf-8">
+    <title>页面重定向</title>
+</head>
+<body>
+<script type="text/javascript">
+    alert('111');
+    window.location = "http://www.baidu.com";
+< /script>
+    
+<?php  
+//$url = "http://www.baidu.com";  
+//echo '<script type="text/javascript">';  
+//echo "window.location.href='$url'";  
+//echo "</script>";  
+?> 
+
+</body>
+</html>
+
+
+
