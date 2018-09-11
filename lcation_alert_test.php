7605dc9f38686c758930f68619108435e90cf739
zhuzhengren <zhuzr@yilan.tv>
2018-09-11 19:41:24 +0800

    0911-1

--- lcation_alert_test.php
+++ lcation_alert_test.php
@@ -1,16 +1,28 @@
 <html>
+    <script type="text/javascript" src="jquery.min.js"></script>
     <body>
         <form action = "welcome.php" method="post">
             名字：<input type="text" name ="fname">
             年龄：<input type="text" name ="age">
-            <input type="submit" value="提交"onclick="test()" onsubmit="return false;" >
+            <input type="submit" value="提交" >       
         </form>
-         <input type="button" value="test"onclick="test()" >
+        <input  type="button" value="test"  onclick="test()">
     </body>
     <script type="text/javascript">
         function test() {
-            alert("ss");
-            window.location.href="https://www.baidu.com";
+           $.ajax({
+                    type: "get",
+                    url: "test2.php",
+                    async: false,
+                    success: function (data) {
+                            alert(data);        
+                    },
+                    error: function (jqXHR, textStatus, errorThrown) {
+                        /*弹出jqXHR对象的信息*/
+                        alert(" ajax error");
+                        alert(jqXHR.responseText);
+                    }
+                })
         }
     </script>
 </html>
--- welcome.php
+++ welcome.php
@@ -1,2 +1,8 @@
+
 欢迎<?php echo $_POST["fname"];?>!<br>
 你的年龄是<?php echo $_POST["age"];?>
+
+<?php
+session_start();
+$_SESSION["age"]=$_POST["age"];
+echo $_SESSION['age'];
